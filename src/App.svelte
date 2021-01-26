cde89c9b452ffff7328c5c2be9dd665d23717acb
Sean Mullen <srmullen13@gmail.com>
2021-01-26 09:32:51 -0500

    update template

--- src/App.svelte
+++ src/App.svelte
@@ -4,9 +4,14 @@
 
 <Tailwind />
 
-<div>
-  <h1 class="text-4xl text-gray-90">Svelte / Typescript / Tailwind / Snowpack</h1>
-  <p class="text-gray-800">
-    A Template
-  </p>
+<div class="bg-gray-50 h-screen">
+  <div class="mx-auto max-w-3xl text-gray-800 py-8 px-4">
+    <h1 class="text-center text-xl sm:text-4xl font-bold mb-8">Svelte / Typescript / Tailwind / Snowpack</h1>
+    <p class="mb-4">
+      A template for a fun, fast development experience!
+    </p>
+    <p>
+      <a class="text-indigo-700 hover:text-indigo-500" href="https://github.com/srmullen/stts">https://github.com/srmullen/stts</a>
+    </p>
+  </div>
 </div>
